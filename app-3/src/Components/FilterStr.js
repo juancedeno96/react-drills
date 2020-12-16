d51b507c0180a0efcfaa33e43c29b5e7f0fd707c
juancedeno96 <cedenojj7@gmail.com>
2020-12-16 16:58:13 -0700

    In progress

---
+++ app-3/src/Components/FilterStr.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+
+class FilterStr extends Component {
+    constructor() {
+        super();
+        this.state = {
+            players: ['Lebron James', 'Steph Curry', 'James Harden', 'Giannis Antetokounmpo', 'Luka Doncic'],
+            userInput: '',
+            filteredPlayers: []
+
+        }
+    }
+
+    handleChange(val) {
+        this.setState({userInput:val })
+    }
+
+    filter(str) {
+        let players = this.state.players
+        let filteredPlayers = []
+        for (let i =0; i<players.length; i++) {
+            if (players[i].includes(str)) {
+                filteredPlayers.push(players[i])
+            }
+        }
+        this.setState({filteredPlayers: filteredPlayers})
+    }
+
+    render() {
+        return (
+            <section>
+                <h2>{this.state.players}</h2>
+                <input onChange={(e)=>this.handleChange(e.target.value)}/>
+                <button onClick={()=>this.filter(this.state.userInput)}>Filter</button>
+                <h2>{this.state.filteredPlayers}</h2>
+
+            </section>
+        )
+    }
+}
+
+export default FilterStr
